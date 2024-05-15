919df3f0109d593bc4b08761422ff570537b8885
ybxzf <42826051+ybxzf@users.noreply.github.com>
2024-05-15 16:45:26 +0800

    Create bottomCenterComp.vue

---
+++ src/views/powerGrid/components/bottomCenterComp.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex-item">
+        <div>
+            bottom-left
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import { ref, reactive, watch, onMounted, onBeforeUnmount } from "vue";
+
+
+const init = async () => { };
+onMounted(() => {
+    init();
+});
+</script>
+<style scoped></style>
