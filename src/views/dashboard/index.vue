0f2a092dc32d9955f845140aaffa7fc5926f0d5d
ybxzf <42826051+ybxzf@users.noreply.github.com>
2024-05-15 16:44:08 +0800

    Create index.vue

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        看板页面开发
+    </div>
+</template>
+<script setup lang="ts">
+import { ref, reactive, watch, onMounted, onBeforeUnmount } from "vue";
+
+
+onMounted(() => {
+    init();
+});
+const init = async () => { };
+</script>
+<style lang="less" scoped></style>
