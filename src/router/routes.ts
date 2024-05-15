13d3184b5554dfb9bd189ba9df75d1a6c51b382a
ybxzf <42826051+ybxzf@users.noreply.github.com>
2024-05-15 16:26:25 +0800

    Update routes.ts

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,12 +1,18 @@
-import Home from "@/views/Home.vue";
+import Home from "@/views/powerGrid/index.vue";
 
 export const routes: any = [
-    {
-        path: '/',
-        redirect: "/home",
-    }, {
-        path: '/home',
-        name: 'home',
-        component: Home,
-    }
-]
+  {
+    path: "/",
+    redirect: "/home",
+  },
+  {
+    path: "/home",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    component: () => import("@/views/dashboard/index.vue"),
+  },
+];
