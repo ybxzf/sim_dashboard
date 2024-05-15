eb93b5eba7f8cd1b30a16f32cea44cab50b2e1e0
ybxzf <42826051+ybxzf@users.noreply.github.com>
2024-05-15 16:25:28 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -16,19 +16,18 @@ body {
 }
 
 #app {
-  height: 100%;
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  // font-family: "Avenir", Helvetica, Arial, sans-serif;
+  // -webkit-font-smoothing: antialiased;
+  // -moz-osx-font-smoothing: grayscale;
+  // color: #2c3e50;
 }
 
-.router-view {
-  width: 100%;
-  height: auto;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  margin: 0 auto;
-}
+// .router-view {
+//   width: 100%;
+//   height: auto;
+//   position: absolute;
+//   top: 0;
+//   bottom: 0;
+//   margin: 0 auto;
+// }
 </style>
