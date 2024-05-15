bf214a1e6a20efd8c54f40aaba6d912503390373
ybxzf <42826051+ybxzf@users.noreply.github.com>
2024-05-15 16:24:50 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { fileURLToPath, URL } from "node:url";
+import pxtorem from "postcss-pxtorem";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   server: {
     host: "0.0.0.0",
     port: 8080,
     proxy: {
-      '/api': 'http://47.108.165.207:20088/ ',
+      "/api": "http://47.108.165.207:20088/ ",
       "/alm_framework": {
         target: "http://47.108.165.207:20088/api",
         timeout: 20 * 60 * 1000,
@@ -24,7 +21,19 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        pxtorem({
+          rootValue: 16, // 指定根元素的字体大小（一般是设计稿宽度的1/10，但设置为16px更方便我自己）
+          propList: ["*"], // 对所有属性进行转换，不进行白名单过滤，如果指定列表，则只对其进行转换
+          // selectorBlackList: ['.no-rem', /^.van-/] // 过滤掉开头是 .no-rem 的class，或者包含 .van- 的class
+          selectorBlackList: [".no-rem"], // 页面中不需要进行rem转换的元素，加个no-rem的classname就行
+        }),
+      ],
+    },
+  },
+});
