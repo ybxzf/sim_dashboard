d928199732aeddac9176f4f9b799514fc513fb14
ybxzf <42826051+ybxzf@users.noreply.github.com>
2024-05-15 16:54:01 +0800

    Create index-CW3dnTUH.js

---
+++ dist/assets/index-CW3dnTUH.js
@@ -0,0 +1,1 @@
+import{d as n,o,c as t,a}from"./index-CT91tAJ8.js";const i=n({__name:"index",setup(c){o(()=>{e()});const e=async()=>{};return(s,r)=>(a(),t("div",null," 看板页面开发 "))}});export{i as default};
