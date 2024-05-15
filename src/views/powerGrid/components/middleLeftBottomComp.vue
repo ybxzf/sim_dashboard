18fb8171d41e08a84817fbfa941a1dbfff0404dd
ybxzf <42826051+ybxzf@users.noreply.github.com>
2024-05-15 16:46:22 +0800

    Create middleLeftBottomComp.vue

---
+++ src/views/powerGrid/components/middleLeftBottomComp.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex-item">
+        <div>middle-left-bottom</div>
+    </div>
+</template>
+<script setup lang="ts">
+import { ref, reactive, watch, onMounted, onBeforeUnmount } from "vue";
+
+
+const init = async () => { };
+onMounted(() => {
+    init();
+});
+</script>
+<style scoped></style>
