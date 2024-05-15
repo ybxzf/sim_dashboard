58df3292fb80dd72a4f7467b34861b3a202258cd
ybxzf <42826051+ybxzf@users.noreply.github.com>
2024-05-15 16:25:47 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,24 @@
-import './assets/base.less';
-import { createApp } from 'vue'
+import "./assets/base.less";
+import { createApp } from "vue";
 
 // 安装pinia
-import { createPinia } from 'pinia'
+import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 
 //引入router
-import router from './router'
+import router from "./router";
 
 //安装element-plus
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
-import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
-import App from './App.vue'
+// 挂载pxToRem到全局
+import "@/utils/rem";
+
+import App from "./App.vue";
 const app = createApp(App);
 
 app.use(pinia);
@@ -23,7 +26,7 @@ app.use(router);
 app.use(ElementPlus);
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
+  app.component(key, component);
 }
 
-app.mount('#app');
+app.mount("#app");
