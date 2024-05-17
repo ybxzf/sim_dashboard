2c4654b51791993022a57d79b022d7cb172f6cd9
ybxzf <42826051+ybxzf@users.noreply.github.com>
2024-05-17 16:57:13 +0800

    Update bottomCenterLeftComp.vue

--- src/views/microPowerGrid/components/bottomCenterLeftComp.vue
+++ src/views/microPowerGrid/components/bottomCenterLeftComp.vue
@@ -5,9 +5,27 @@
                 <span>中央空调实时监控</span>
             </div>
             <div class="item-content">
-                <div class="content-title">运行概况</div>
+                <div class="content-title">
+                    <img src="../../../assets//images//microPowerGrid/mini_title.png">
+                    <span>运行概况</span>
+                </div>
                 <div class="content-cnt">
-
+                    <div class="content-cnt-item">
+                        <div style="flex:1">1.1</div>
+                        <div style="flex:2">1.2</div>
+                    </div>
+                    <div class="content-cnt-item">
+                        <div style="flex:1">1.1</div>
+                        <div style="flex:2">1.2</div>
+                    </div>
+                    <div class="content-cnt-item">
+                        <div style="flex:1">1.1</div>
+                        <div style="flex:2">1.2</div>
+                    </div>
+                    <div class="content-cnt-item">
+                        <div style="flex:1">1.1</div>
+                        <div style="flex:2">1.2</div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -40,19 +58,50 @@ const init = async () => { };
         }
 
         .item-content {
-            width: 100%;
+            width: 30rem;
             height: 16.7rem;
             // border: 1px solid red;
 
             .content-title {
-                border: 1px solid;
+                // border: 1px solid #fff;
                 height: 1.7rem;
+                position: relative;
+                padding-left: 2rem;
+                font-style: italic;
+                font-weight: bolder;
+                color: #fff;
+                background: linear-gradient(to bottom, white, rgb(148, 203, 255));
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+
+                img {
+                    position: absolute;
+                    width: 20rem;
+                    left: 1.2rem;
+                }
             }
 
             .content-cnt {
-                border: 1px solid;
-                height: 15rem;
+                // border: 1px solid #fff;
+                background-color: #fff;
 
+                height: 15rem;
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: center;
+
+                .content-cnt-item {
+                    border: 1px solid;
+                    flex-basis: 13rem;
+                    display: flex;
+                    flex-wrap: wrap;
+                    justify-content: center;
+
+                    >* {
+                        border: 1px solid;
+                        // flex-basis: 6rem;
+                    }
+                }
             }
         }
     }
