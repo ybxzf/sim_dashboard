2de046d58274a145021d6c7833f495a79b7a5b42
ybxzf <42826051+ybxzf@users.noreply.github.com>
2024-05-28 17:23:16 +0800

    Update microPowerGridServer.ts

--- src/utils/api/microPowerGridServer.ts
+++ src/utils/api/microPowerGridServer.ts
@@ -1,8 +1,17 @@
 import axios from 'axios';
+import $axios from '@/utils/axios';
 
 export function getYesterdayElectricity() {
     return axios.request({
         url: '/api/bigScreen/yesterdayElectricity',
         method: 'get',
+    })
+}
+
+export function getDevEnvList(reqParam:Object) {
+    return $axios({
+        method: 'post',
+        url: '/api/bigScreen/yesterdayElectricity',
+        data: reqParam
     })
 }
